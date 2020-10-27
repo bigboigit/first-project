b98086afd18ef4cd4ec0c002b44fbcb24dcc504b
unknown <kiarash0108@icloud.com>
2020-10-27 14:51:09 -0700

    commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# Deleting everything, Cuz its garbage
-
+# Bubble Sort Algo
+def bubblesort(array):
+    for element in range(len(array)):
+        for elements in range(len(array)-1):
+            if array[elements] > array[elements+1]:
+                array[elements], array[elements+1] = array[elements+1], array[elements]
+    return array
--- main2.py
+++
@@ -1,1 +0,0 @@
-# Smae here
