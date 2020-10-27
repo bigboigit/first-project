6ed008af6a6eb64901b0364e0fecab8d0141ac3e
unknown <kiarash0108@icloud.com>
2020-10-27 14:48:36 -0700

    commit

--- main2.py
+++ main2.py
@@ -1,9 +1,1 @@
-# List of sorting algos
-
-def bubblesort(ar):
-	for i in range(len(ar)):
-		for j in range(i, len(ar) - 1):
-			if ar[j] > ar[j+1]:
-				ar[j], ar[j+1] = ar[j+1], ar[j]
-	return ar
-print(bubblesort([1000, 10, 20]))
+# Smae here
